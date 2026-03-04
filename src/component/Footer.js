379cc3bbad2a08a83a516fb7d7a560b30a364d5b
rocko1-admin <melq.oax@gmail.com>
2026-03-03 23:40:05 -0600

    Pie de  pagina para mostrar informcaión del diseño de lapagina, pendiente desarrollo

---
+++ src/component/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "../styles/styles.css";
+import "../styles/colors.css";
+
+export default function footer (){
+  return (
+    <footer className="footer">
+      <h2>Aqui van los creditos</h2>
+    </footer>
+  )
+}
