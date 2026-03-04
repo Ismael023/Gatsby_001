36158a017a2ebe6e3bb8e4c2177fbe16dc3233c5
rocko1-admin <melq.oax@gmail.com>
2026-03-03 23:40:38 -0600

    paginas de contactos y portafolio, pendiente desarrollo, solo se uso para trabajar la ruta de los archivos

---
+++ src/pages/contact.js
@@ -0,0 +1,16 @@
+import React from "react"
+import Layout from "../component/LayoutPage"
+
+
+
+export default function contact (){
+  return (
+    <Layout>
+     
+        <h1>Page Contacto con imagenes</h1>
+     
+    </Layout>
+  )
+}
+
+//export default contact 
---
+++ src/pages/portfolio.js
@@ -0,0 +1,13 @@
+//import * as React from "react";
+
+import React from "react";
+
+import Layout from "../component/LayoutPage"
+
+export default function portfolio(){
+  return ( 
+    <Layout>
+      <h1>Portafolio</h1>
+    </Layout>
+  )
+}
