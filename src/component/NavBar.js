74949c7cd7b524b119f5f9b9c261f2fee6377659
rocko1-admin <melq.oax@gmail.com>
2026-03-03 23:42:38 -0600

    barra de navegación para trabajar con rutas de archivos y uso de Link, pendiente el uso de estados y estilos en css

---
+++ src/component/NavBar.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { Link } from "gatsby";
+
+import  "../styles/styles.css";
+import  "../styles/colors.css";
+
+const links =[  {name: 'Home', href: '/'},
+             {name: 'Portafolio', href: '/portfolio'},
+             {name: 'Contacto', href: '/contact'}];
+
+export default function navBar() {
+  return(
+    <nav className="navbar">
+      <div className="menu">
+      {/* menu en pantalla grande */}        
+      {links.map( (link) => { 
+        return (
+          <Link to={link.href}
+            className="linkMenu"
+            >
+            {link.name}
+          </Link>
+        )
+      } ) }
+      </div>
+
+      {/* boton sección movil */}
+      <button className="btnHamburguesa"> 
+       <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30" fill="#000">
+            <path d="M 3 7 A 1.0001 1.0001 0 1 0 3 9 L 27 9 A 1.0001 1.0001 0 1 0 27 7 L 3 7 z M 3 14 A 1.0001 1.0001 0 1 0 3 16 L 27 16 A 1.0001 1.0001 0 1 0 27 14 L 3 14 z M 3 21 A 1.0001 1.0001 0 1 0 3 23 L 27 23 A 1.0001 1.0001 0 1 0 27 21 L 3 21 z"></path>
+        </svg>
+      </button>
+
+      
+      <div className="menuMovil">
+        {links.map( (link) => { 
+          return (
+            <Link to={link.href} 
+              className="linkMenu"
+              >
+              {link.name}
+            </Link>
+          )
+        } ) }
+      </div>
+      
+      
+    </nav>
+  )
+}
