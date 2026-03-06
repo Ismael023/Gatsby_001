a698a5019d25b250165e21f5abd50ef6be0f8ecc
rocko1-admin <melq.oax@gmail.com>
2026-03-06 12:38:24 -0600

    se edito leyenda y se elimino comentarios

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -5,7 +5,7 @@ import "../styles/colors.css";
 
 export default function footer (){
   return (
-    <footer className="footer">
+    <footer > { /* className="footer" */}
       <h4>© 2026 Ismael Ramírez Díaz</h4>
     </footer>
   )
