3b493439b27fc0aba30811581b347ea38dcf2dea
rocko1-admin <melq.oax@gmail.com>
2026-04-22 18:59:01 -0600

    chore: eliminacion de comentarios

--- src/component/footer/index.js
+++ src/component/footer/index.js
@@ -1,16 +1,6 @@
 import React from "react";
 import * as styles from "./styles.css"
 
-/*
-export default function Footer(){
-  return (
-    <footer className={styles.footer}>
-      <div>Copyright 2026 ©  Ismael Ramírez Díaz.</div>
-    </footer>
-  )
-}
-*/
-
 const Footer = () => {
   return (
     <footer>
