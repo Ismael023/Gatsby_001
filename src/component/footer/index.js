3b7d03590f2690b352230671a131af5fdfdfad48
rocko1-admin <melq.oax@gmail.com>
2026-04-22 18:49:50 -0600

    chore: refactor de nuevo componente footer

---
+++ src/component/footer/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import * as styles from "./styles.css"
+
+/*
+export default function Footer(){
+  return (
+    <footer className={styles.footer}>
+      <div>Copyright 2026 ©  Ismael Ramírez Díaz.</div>
+    </footer>
+  )
+}
+*/
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>Copyright 2026 ©  Ismael Ramírez Díaz.</div>
+    </footer>
+  )
+}
+
+export default Footer;
