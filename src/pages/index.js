16b25b8ccd24cecebf6dcde7f72f40b2732039b2
rocko1-admin <melq.oax@gmail.com>
2026-05-22 00:09:40 -0600

    chore: se ajusto el nombre de las variables que llaman a los colores y se ajusto el llamado a los colores establecidos

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import  React, { useState, useEffect } from "react"
 
 import Layout from "../component/Layout"
 import * as styles  from "../styles/home.module.css"
+                                                          import "../styles/colors.css"
 //import { useState, useEffect } from "react"
 import image from "../images/logo-SF_02.png"
 
@@ -24,12 +25,11 @@ const IndexPage = () => {
       <section className={styles.header} >
         <h2>ISMAEL RAMÍREZ DÍAZ</h2>
         <div>
-        <img className={styles.logoHome} src={image} alt="logo" ></img>
-        <h3>Soluciones Integrales <br></br>para tu{" "} 
-          <span key={indice} className={styles.rotative}>{servicio[indice]}
-          </span> 
-        </h3>
-
+          <img className={styles.logoHome} src={image} alt="logo" ></img>
+          <h3>Soluciones Integrales <br></br>para tu{" "} 
+            <span key={indice} className={styles.rotative}>{servicio[indice]}
+            </span> 
+          </h3>
         </div>
         
         <p>Consultor con mas de <span>15</span> años de experiencia en el sector de la informatica y el soporte tecnico.</p>
